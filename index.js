e3acd9c692a8b0c4092c8cb3c111be27e6b2f4b1
MuhammadSalah99 <mohmmedjunide73@gmail.com>
2023-05-19 00:42:12 +0300

    setting up the postgress

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const db = require("./models")
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-const express = require("express")
-const dotenv = require("dotenv")
+app.use(cors(corsOptions));
 
-dotenv.config()
+// parse requests of content-type - application/json
+app.use(express.json());
 
-const app = express()
-const port = process.env.PORT;
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
-    res.send("Hi from backend")
-})
+db.sequelize.sync({force: true})
+    .then(()=> {
+        console.log("synced db")
+    })
+    .catch((err)=> {
+        console.log("failed to sync db: " + err.message)
+    })
 
-app.listen(port, ()=>{
-     console.log(`[server]: server is running at port`);
-})
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
---
+++ models/blogs.model.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, Sequelize) => {
+    const Blog = sequelize.define("blogs", {
+        title: {
+            type: Sequelize.STRING
+        },
+        description: {
+            type: Sequelize.STRING
+        },
+        published: {
+            type: Sequelize.BOOLEAN
+        }
+    });
+
+    return Blog
+
+};
